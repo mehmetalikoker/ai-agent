d9773b69564c437c6f5f640a086017f62f782562
Mehmet <mehmetalikoker@outlook.com>
2026-03-09 19:25:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from sympy import content
+
+load_dotenv()
+
+aimodel = ChatOpenAI(model="gpt-4")
+
+
+search = TavilySearchResults(max_results=2)
+tavilysearch = [search]
+
+modelEngine = aimodel.bind_tools(tavilysearch)
+
+if __name__ == "__main__":
+    response = modelEngine.invoke([HumanMessage(content="what is Mehmet Ali Köker job")])
+    print(response)
